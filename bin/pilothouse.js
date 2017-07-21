3e35b0893aeb790b7889b0e379a8153f82fa14cf
Philip Newcomer <contact@philipnewcomer.net>
2017-07-21 16:29:48 -0400

    Update documentation

--- bin/pilothouse.js
+++ bin/pilothouse.js
@@ -4,6 +4,7 @@ const yargs = require('yargs');
 
 const program = yargs
 	.commandDir('../commands/')
+	.epilogue('See http://pilothouse-app.org/ for complete documentation and usage instructions.')
 	.help()
 	.usage('$0 <command> [args]')
 	.version()
