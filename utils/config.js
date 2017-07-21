bc9f98743cb234eb3e0f2e18ae513deee2a44524
Philip Newcomer <contact@philipnewcomer.net>
2017-07-21 19:13:02 -0400

    Allow override of all bundled config files

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,7 @@
 const environment = require('./environment'),
       fs = require('fs-extra'),
-      helpers = require('./helpers');
+      helpers = require('./helpers'),
+      path = require('path');
 
 let config = helpers.readYamlConfig(environment.appHomeDirectory + '/config.yml', getDefaultConfig());
 config.composeVariables = getComposeVariables();
@@ -19,22 +20,37 @@ module.exports = config;
  * @returns {Object}
  */
 function getComposeVariables() {
-	const appDirectory = environment.appDirectory;
-
 	return {
 		'HTTPS_CERTIFICATE_CERT': environment.httpsCertificateCertPath,
 		'HTTPS_CERTIFICATE_KEY': environment.httpsCertificateKeyPath,
-		'MYSQL_CONFIG_FILE': appDirectory + '/config/mysql/mysql.conf',
+		'MYSQL_CONFIG_FILE': getConfigFilePath('mysql.conf'),
 		'NGINX_COMPILED_SITES_CONFIG_FILE': environment.runDirectory + '/nginx-compiled-sites.conf',
-		'NGINX_CONFIG_FILE': appDirectory + '/config/nginx/nginx.conf',
-		'NGINX_DEFAULT_SITE_CONFIG_FILE': appDirectory + '/config/nginx/default-site.conf',
-		'PHP_CONFIG_FILE': appDirectory + '/config/php-fpm/php.ini',
-		'PHP_FPM_CONFIG_FILE': appDirectory + '/config/php-fpm/php-fpm.conf',
-		'PHP_XDEBUG_CONFIG_FILE': appDirectory + '/config/php-fpm/xdebug.ini',
+		'NGINX_CONFIG_FILE': getConfigFilePath('nginx.conf'),
+		'NGINX_DEFAULT_SITE_CONFIG_FILE': getConfigFilePath('nginx-default-site.conf'),
+		'PHP_CONFIG_FILE': getConfigFilePath('php.ini'),
+		'PHP_FPM_CONFIG_FILE': getConfigFilePath('php-fpm.conf'),
+		'PHP_XDEBUG_CONFIG_FILE': getConfigFilePath('xdebug.ini'),
 		'SITES_DIR': config.sites_dir,
-		'SSMTP_CONFIG_FILE': appDirectory + '/config/ssmtp/ssmtp.conf',
-		'WPCLI_CONFIG_FILE': appDirectory + '/config/wp-cli/wp-cli.yml',
+		'SSMTP_CONFIG_FILE': getConfigFilePath('ssmtp.conf')
 	}
+}
+
+/**
+ * Returns the path for the specified config file.
+ *
+ * Will return the path to the override file instead of the bundled file, if it exists.
+ *
+ * @param {String} filename The config filename.
+ *
+ * @returns The full path to the config file.
+ */
+function getConfigFilePath(filename) {
+
+	if (fs.existsSync(path.join(environment.appHomeDirectory, filename))) {
+		return path.join(environment.appHomeDirectory, filename);
+	}
+
+	return path.join(environment.appDirectory, 'config', filename);
 }
 
 /**
